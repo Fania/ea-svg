4da49110e2ef6f20a338fe93ebcd007896df5c58
DaveEveritt <deveritt@innotts.co.uk>
2017-12-25 21:53:17 +0000

    add woff font, one path to ploygon, gradients to css

--- scripts.js
+++ scripts.js
@@ -1,25 +1,32 @@
 (function(){
 
-  let raysButton = document.getElementById("rays-button");
-  let planetsButton = document.getElementById("planets-button");
-  let nonsacredsButton = document.getElementById("nonsacred-button");
+  // let raysButton = document.getElementById("rays-button");
+  // let planetsButton = document.getElementById("planets-button");
+  // let nonsacredsButton = document.getElementById("nonsacred-button");
 
-  let rays = document.getElementsByClassName("ray");
-  let planets = document.getElementsByClassName("planet");
-  let nonsacreds = document.getElementsByClassName("nonsacred");
+  // let rays = document.getElementsByClassName("ray");
+  // let planets = document.getElementsByClassName("planet");
+  // let nonsacreds = document.getElementsByClassName("nonsacred");
 
-  let choicesForm = document.getElementById("choices");
-  let radios = document.getElementsByClassName("choice");
+  // let choicesForm = document.getElementById("choices");
+  // let radios = document.getElementsByClassName("choice");
 
+  // radios.addEventListener("change", function(){
+  //   console.log("changed");
+  // });
 
-  raysButton.addEventListener("change", function(){
-    
-    console.log("changed");
-
-
-  });
-
-
+  let rad = document.choices.mapChoice;
+  console.log(rad);
+  var prev = null;
+  for(var i = 0; i < rad.length; i++) {
+    rad[i].onchange = function() {
+      (prev)? console.log(prev.value):null;
+      if(this !== prev) {
+        prev = this;
+      }
+      console.log(this.value)
+    };
+  }
 
 
   // if (raysButton.checked) {
@@ -60,10 +67,6 @@
   //     r.classList.remove("hide");
   //   }
   // }
-
-
-
-
 
 
 })();
