3b52bec59e938bc31d4ea4532549469afdc27ca1
Fania Ra <fania@web.de>
2019-08-14 20:01:03 +0100

    hori vert separate

--- scripts.js
+++ scripts.js
@@ -1,72 +1,79 @@
-(function(){
-
-  // let raysButton = document.getElementById("rays-button");
-  // let planetsButton = document.getElementById("planets-button");
-  // let nonsacredsButton = document.getElementById("nonsacred-button");
-
-  // let rays = document.getElementsByClassName("ray");
-  // let planets = document.getElementsByClassName("planet");
-  // let nonsacreds = document.getElementsByClassName("nonsacred");
-
-  // let choicesForm = document.getElementById("choices");
-  // let radios = document.getElementsByClassName("choice");
-
-  // radios.addEventListener("change", function(){
-  //   console.log("changed");
-  // });
-
-  let rad = document.choices.mapChoice;
-  console.log(rad);
-  var prev = null;
-  for(var i = 0; i < rad.length; i++) {
-    rad[i].onchange = function() {
-      (prev)? console.log(prev.value):null;
-      if(this !== prev) {
-        prev = this;
-      }
-      console.log(this.value)
-    };
-  }
+const dia = document.getElementById("diagram");
+const mql = window.matchMedia('(min-width: 600px)');
+function screenTest(e) {
+  const size = e.matches ? 'desktop' : 'mobile';
+  console.log(size);
+  dia.src = e.matches ? "horizontal.svg" : "vertical.svg";
+}
+mql.addListener(screenTest);
+screenTest(mql);
 
 
-  // if (raysButton.checked) {
-  //   console.log("raysButton");
-  //   for (let r in rays) {
-  //     r.classList.remove("hide");
-  //   }
-  //   for (let p in planets) {
-  //     p.classList.add("hide");
-  //   }
-  //   for (let r in nonsacreds) {
-  //     r.classList.add("hide");
-  //   }
-  // }
 
-  // if (planetsButton.checked) {
-  //   console.log("planetsButton");
-  //   for (let r in rays) {
-  //     r.classList.add("hide");
-  //   }
-  //   for (let p in planets) {
-  //     p.classList.remove("hide");
-  //   }
-  //   for (let r in nonsacreds) {
-  //     r.classList.add("hide");
-  //   }
-  // }
+// let raysButton = document.getElementById("rays-button");
+// let planetsButton = document.getElementById("planets-button");
+// let nonsacredsButton = document.getElementById("nonsacred-button");
 
-  // if (nonsacredsButton.checked) {
-  //   console.log("nonsacredsButton");
-  //   for (let r in rays) {
-  //     r.classList.add("hide");
-  //   }
-  //   for (let p in planets) {
-  //     p.classList.add("hide");
-  //   }
-  //   for (let r in nonsacreds) {
-  //     r.classList.remove("hide");
-  //   }
-  // }
+// let rays = document.getElementsByClassName("ray");
+// let planets = document.getElementsByClassName("planet");
+// let nonsacreds = document.getElementsByClassName("nonsacred");
+
+// let choicesForm = document.getElementById("choices");
+// let radios = document.getElementsByClassName("choice");
+
+// radios.addEventListener("change", function(){
+//   console.log("changed");
+// });
+
+let rad = document.choices.mapChoice;
+console.log(rad);
+var prev = null;
+for(var i = 0; i < rad.length; i++) {
+  rad[i].onchange = function() {
+    (prev)? console.log(prev.value):null;
+    if(this !== prev) {
+      prev = this;
+    }
+    console.log(this.value)
+  };
+}
 
 
-})();
+// if (raysButton.checked) {
+//   console.log("raysButton");
+//   for (let r in rays) {
+//     r.classList.remove("hide");
+//   }
+//   for (let p in planets) {
+//     p.classList.add("hide");
+//   }
+//   for (let r in nonsacreds) {
+//     r.classList.add("hide");
+//   }
+// }
+
+// if (planetsButton.checked) {
+//   console.log("planetsButton");
+//   for (let r in rays) {
+//     r.classList.add("hide");
+//   }
+//   for (let p in planets) {
+//     p.classList.remove("hide");
+//   }
+//   for (let r in nonsacreds) {
+//     r.classList.add("hide");
+//   }
+// }
+
+// if (nonsacredsButton.checked) {
+//   console.log("nonsacredsButton");
+//   for (let r in rays) {
+//     r.classList.add("hide");
+//   }
+//   for (let p in planets) {
+//     p.classList.add("hide");
+//   }
+//   for (let r in nonsacreds) {
+//     r.classList.remove("hide");
+//   }
+// }
